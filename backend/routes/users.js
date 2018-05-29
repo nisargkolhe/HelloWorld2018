f0c3cc6966931fcfa071ea52321a586413edc28a
Nisarg Kolhe <nisargk@fb.com>
2018-05-28 22:50:06 -0700

    Added services

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({ 
+    password: String, 
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    roles: [],
+    verified: Boolean
+}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
