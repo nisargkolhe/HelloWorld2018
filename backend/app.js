0cce4e7500dbd6a796b0e6135c4d923d249c95a6
Nisarg Kolhe <nisargkolhe@gmail.com>
2018-07-08 19:33:20 -0700

    Fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,12 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.options('*', cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //app.use('/', routes);
 app.use('/user', user);
 app.use('/exec', exec);
